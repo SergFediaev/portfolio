ea446c47d8e4c8325498a778a5466b8d6d6b7a3e
SergFediaev <sergfediaev@gmail.com>
2024-01-27 18:47:55 +0300

    Подключен шрифт, почищены комментарии.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {GlobalStyled} from "./styles/Global.styled";
+import './index.css';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
